4079b7682975990f35459664db45ec7727bdecfd
Elif Vural <elifvural019@gmail.com>
2024-10-16 09:57:39 +0300

    var name ingredient and image added

--- app/src/main/java/com/example/cookbookapp/model/Recipe.kt
+++ app/src/main/java/com/example/cookbookapp/model/Recipe.kt
@@ -1,4 +1,18 @@
 package com.example.cookbookapp.model
 
-class Recipe {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+
+
+@Entity
+data class Recipe(
+
+    @ColumnInfo(name= "name")
+    var name: String,
+
+    @ColumnInfo(name="ingredient")
+    var ingredient: String,
+
+    @ColumnInfo(name="image")
+    var image: ByteArray
+)

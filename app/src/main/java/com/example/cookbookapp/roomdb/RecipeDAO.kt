5f4f05f0f6eb36478e829fbad28e8b11e8f8ca63
Elif Vural <elifvural019@gmail.com>
2024-10-16 10:03:57 +0300

    commits on RecipeDAO

--- app/src/main/java/com/example/cookbookapp/roomdb/RecipeDAO.kt
+++ app/src/main/java/com/example/cookbookapp/roomdb/RecipeDAO.kt
@@ -1,4 +1,22 @@
 package com.example.cookbookapp.roomdb
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.cookbookapp.model.Recipe
+
 interface RecipeDAO {
+
+    @Query("SELECT*FROM Recipe")
+    fun getAll(): List<Recipe>
+
+    @Query("SELECT*FROM Recipe WHERE id=id")
+    fun findById(id: Int): Recipe
+
+    @Insert
+    fun insert(recipe: Recipe)
+
+    @Delete
+    fun delete(recipe: Recipe)
+
 }
